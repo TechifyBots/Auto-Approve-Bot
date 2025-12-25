de1bc299daf19232a6d59ce6fb27a2ab411fc765
Techify Bots <TechifyBots@gmail.com>
2025-12-25 15:26:14 +0530

    Update callback.py

--- TechifyBots/callback.py
+++ TechifyBots/callback.py
@@ -1,24 +1,31 @@
+import random
 from pyrogram import Client
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from Script import text
-from config import ADMIN
+from config import ADMIN, PICS
 
 @Client.on_callback_query()
 async def callback_query_handler(client, query: CallbackQuery):
     if query.data == "start":
-        await query.message.edit_caption(
-            caption=text.START.format(query.from_user.mention),
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.START.format(query.from_user.mention)
+            ),
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('⇆ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ⇆', url=f"https://telegram.me/QuickAcceptBot?startgroup=true&admin=invite_users")],
+                [InlineKeyboardButton('⇆ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ⇆', url="https://telegram.me/QuickAcceptBot?startgroup=true&admin=invite_users")],
                 [InlineKeyboardButton('ℹ️ 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
                  InlineKeyboardButton('📚 𝖧𝖾𝗅𝗉', callback_data='help')],
-                [InlineKeyboardButton('⇆ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 ⇆', url=f"https://telegram.me/QuickAcceptBot?startchannel=true&admin=invite_users")]
+                [InlineKeyboardButton('⇆ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 ⇆', url="https://telegram.me/QuickAcceptBot?startchannel=true&admin=invite_users")]
             ])
         )
 
     elif query.data == "help":
-        await query.message.edit_caption(
-            caption=text.HELP.format(query.from_user.mention),
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.HELP.format(query.from_user.mention)
+            ),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://telegram.me/Techifybots'),
                  InlineKeyboardButton('💬 𝖲𝗎𝗉𝗉𝗈𝗋𝗍', url='https://telegram.me/TechifySupport')],
@@ -28,8 +35,11 @@ async def callback_query_handler(client, query: CallbackQuery):
         )
 
     elif query.data == "about":
-        await query.message.edit_caption(
-            caption=text.ABOUT,
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.ABOUT
+            ),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 👨‍💻', user_id=int(ADMIN))],
                 [InlineKeyboardButton("↩️ 𝖡𝖺𝖼𝗄", callback_data="start"),
